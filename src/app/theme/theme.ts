2da0ff513927280d8f74a619d2a575615509cd8a
lungjoke <phansak.s@kkumail.com>
2024-06-23 01:23:59 +0700

    next-store

---
+++ src/app/theme/theme.ts
@@ -0,0 +1,31 @@
+'use client';
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+import {Colors} from './colors'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+const roboto = Roboto({
+    weight: ['300', '400', '500', '700'],
+    subsets: ['latin'],
+    display: 'swap',
+  });
+
+  const theme = createTheme({
+    palette: {
+      mode: 'light',
+      primary:{
+        main:Colors.primary
+      },
+      secondary:{
+        main:Colors.secondary
+      },
+    },
+    typography: {
+        fontFamily: roboto.style.fontFamily,
+       },
+  });
+  
+  export default theme;
